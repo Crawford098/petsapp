8d19a25c8d382fa0bb979e7c5c2dc60f8bbe3f95
Jonathan <jonathan@Jonathans-iMac.local>
2024-07-05 14:01:22 -0400

    Working with repository

--- src/main/java/org/santana/repository/Repository.java
+++ src/main/java/org/santana/repository/Repository.java
@@ -1,6 +1,10 @@
 package org.santana.repository;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 import org.santana.config.database.MysqlConnections;
 import org.santana.model.Model;
@@ -19,13 +23,63 @@ public class Repository {
         }
     }
 
-    public void findById() {
-        
-    //todo: probar la base de datos.
+    public void findById(int $id) {
+
+        // PreparedStatement preparedStatement = this.db.prepareStatement("");
+        //todo: probar la base de datos.
     }
 
     public void findAll() {
 
     }
 
+    //Todo: Define the datatable prefix.
+    //Todo: Add dinamic values en sql query
+    //Todo: Add dinamic values in preparedStament
+    //Todo: Add validtions (check it)
+    public boolean save(Object data) {
+
+        boolean result = false;
+        String columns = this.getModelColumns();
+        String values = this.getModelValues(data);
+        String sql = "INSERT INTO pe_" + this.model.getClass() + " (" + columns + ") VALUES (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = this.db.prepareStatement(sql);
+
+            preparedStatement.setString(1, "jonathan");
+            preparedStatement.setString(2, "prueba@gmail.com");
+            preparedStatement.setString(3, "123456");
+            preparedStatement.setString(4, LocalDate.now().toString());
+
+            result = (preparedStatement.executeUpdate() != 0);
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private String getModelColumns() {
+        //Get field list of the class.
+        Field[] fields = this.model.getClass().getDeclaredFields();
+
+        String columns = "";
+
+        //Converting the array of fields in a String separed by commas.
+        for (Field field : fields) {
+            columns += field.getName() + ", ";
+        }
+
+        return columns.substring(0, columns.length() - 1);
+    }
+
+    private String getModelValues(Object data) {
+        Field[] fields = this.model.getClass().getDeclaredFields();
+
+        
+        return "";
+    }
 }
--- src/main/java/org/santana/service/users/Users.java
+++ src/main/java/org/santana/service/users/Users.java
@@ -5,10 +5,12 @@ public class Users {
 
     private String username;
     private String password;
+    private String email;
 
-    public Users(String username, String password) {
+    public Users(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
 
     public String getUsersname() {
@@ -23,6 +25,14 @@ public class Users {
         return this.password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -32,4 +42,5 @@ public class Users {
         sb.append('}');
         return sb.toString();
     }
+
 }
