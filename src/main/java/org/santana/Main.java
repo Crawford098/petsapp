e93308aa0dc89e1e6f50f9d2bc261f3d0ba961ea
Jonathan Santana <santanajonathan098@gmail.com>
2024-09-08 23:36:22 -0700

    Actualizando proyecto

--- src/main/java/org/santana/Main.java
+++ src/main/java/org/santana/Main.java
@@ -6,38 +6,37 @@ import org.santana.service.auth.UserAuth;
 import org.santana.service.users.Users;
 
 class Main {
-    public static void main (String [] args)
-    {
+    public static void main(String[] args) {
 
         System.out.println("Welcome to petShop App");
         System.out.println("Do you what to login the app?");
         System.out.println("Yes or No");
 
-        //create Scanner Object to capture the result
+        // create Scanner Object to capture the result
         Scanner scanner = new Scanner(System.in);
 
-        //Capturing the result
+        // Capturing the result
         String result = scanner.nextLine();
 
-        if("yes".equals(result))
-        {
+        if ("yes".equals(result)) {
             System.out.println("Type your username:");
             String username = scanner.nextLine();
 
             System.out.println("Type your password");
             String password = scanner.nextLine();
 
-            Users user = new Users(username, password, "prueba@prueba.com");
+            Users user = new Users();
+            user.setUsername("Jonathan");
+            user.setEmail("prueba@gmail.com");
+            user.setPassword("123456");
             UserAuth auth = new UserAuth();
 
             System.out.println(auth.register(user));
             System.out.println(auth.login(user));
             System.out.println(user.toString());
-        }
-        else
-        {
+        } else {
             System.out.println("See you later.");
         }
     }
 
-} 
+}
